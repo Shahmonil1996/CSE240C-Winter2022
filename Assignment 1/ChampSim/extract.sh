91b8565155ec1dd18ad5635611957fa8316cc885
Monil Shah <shahmonil1996@gmail.com>
2022-01-27 19:22:06 -0800

    Added Steps to Run the Setup
    1. result file added
    2. steps to run added
    3. Additional redundant and useless files removed
    4, HW pdf added

--- Assignment 1/ChampSim/extract.sh
+++ Assignment 1/ChampSim/extract.sh
@@ -2,10 +2,10 @@
 SECONDS=0
 for i in DJOLT_Design_10long_4short DJOLT_Design_20long_4short DJOLT_Design_2short_15long DJOLT_Design_6short_15long DJOLT_Design_10short_15long DJOLT_Design_25long_4short DJOLT_Design_30long_4short DJOLT_Design_8short_15long; do
   cd $i
-  #grep -nr "Finished CPU 0 instructions" | tee grep.txt
-  #grep -nr "Finished CPU 0 instructions" -nr grep.txt | cut -d " " -f10 | tee grep.csv
-  #grep -nr "L1I LOAD" | tee grep2.txt
-  #grep -nr "L1I LOAD" grep.txt | cut -d ":" -f7 | tee grep2.csv
+  grep -nr "Finished CPU 0 instructions" | tee grep.txt
+  grep -nr "Finished CPU 0 instructions" -nr grep.txt | cut -d " " -f10 | tee grep.csv
+  grep -nr "L1I LOAD" | tee grep2.txt
+  grep -nr "L1I LOAD" grep.txt | cut -d ":" -f7 | tee grep2.csv
   echo "in directory $i"
   cd ..
 done
